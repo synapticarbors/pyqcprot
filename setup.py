daf534373038301abdc6b2cfe18b2e322826e330
Joshua Adelman <joshua.adelman@gmail.com>
2012-06-16 21:00:12 -0400

    Cleaned up setup.py to automatically decide whether to use cython or generate module from the .c file

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
 import numpy
 try:
@@ -8,13 +7,23 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
+# Handle cython modules
+try:
+    from Cython.Distutils import build_ext
+    use_cython = True
+    cmdclass = {'build_ext': build_ext}
+except ImportError:
+    use_cython = False
+    cmdclass = {}
+finally:
+    print 'use_cython: {}'.format(use_cython)
 
-ext_modules = [Extension("pyqcprot", ["pyqcprot.pyx"],
+ext_modules = [Extension("pyqcprot", ["pyqcprot.{}".format('pyx' if use_cython else 'c')],
                 include_dirs=[numpy_include],
                 extra_compile_args=["-O3","-ffast-math"])]
 
 setup(
   name = 'Python qcprot module',
-  cmdclass = {'build_ext': build_ext},
+  cmdclass = cmdclass,
   ext_modules = ext_modules
 )
